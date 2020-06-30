93fc5c6297ae1a9a4a75e50bdb2a928db0849ab0
soufiane <soufiianebenalla@gmail.com>
2020-06-30 08:29:47 -0700

    the first View and Template for timezone

---
+++ products/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.utils import timezone
+
+
+# Create your views here.
+
+def say_hi(request, name):
+    return render(request, 'say_hi.html', {'name': name})
+
+def show_time(request):
+	now = timezone.now()
+	return render(request, 'show_time.html', {'now':now})
--- store/urls.py
+++ store/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from products.views import say_hi, show_time
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('say_hi/<str:name>', say_hi),
+    path('show_time', show_time)
 ]
