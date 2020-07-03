ed0d83939866d0f56870d9d6b6de33858df3b3e4
soufiane <soufiianebenalla@gmail.com>
2020-07-03 06:22:37 -0700

    Add first project to heroku!

--- store/settings.py
+++ store/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -119,3 +120,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+# herokuapp Add
+
+ALLOWED_HOSTS = ['.herokuapp.com', ]
