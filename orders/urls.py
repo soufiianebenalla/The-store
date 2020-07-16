820b2354251c53d29abd64ef158da7a495def2ce
soufiane <soufiianebenalla@gmail.com>
2020-07-16 15:55:34 -0700

    Add The Cart and the Order at the store

--- orders/urls.py
+++ orders/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from . import views
+from .views import order_create
 
 
 app_name = 'orders'
 
 
 urlpatterns = [
-    path('create/', views.order_create, name='order_create'),
+    path('create/', order_create, name='order_create'),
 ]
