9f5fad94dbc9562e0cd48fd13d3a6a097f9ea7a8
soufiane <soufiianebenalla@gmail.com>
2020-07-21 16:13:12 -0700

    Change DEBUG to False

--- store/prod_settings.py
+++ store/prod_settings.py
@@ -7,7 +7,7 @@ from .settings import *
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['.herokuapp.com']
 
