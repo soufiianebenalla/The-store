b5e9de7ec03ce9eff1d30917c3184c5fe6f35bb0
soufiane <soufiianebenalla@gmail.com>
2020-07-21 15:47:31 -0700

    Add database config

--- store/prod_settings.py
+++ store/prod_settings.py
@@ -1,11 +1,18 @@
 import os
+import dj_database_url
 
 from .settings import *
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY =
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = ['.herokuapp.com']
+
+DATABASE_URL = environ.get('DATABASE_URL')
+
+DATABASES = {
+    'default': dj_database_url.config(os.environ.get('DATABASE_URL'))
+}
